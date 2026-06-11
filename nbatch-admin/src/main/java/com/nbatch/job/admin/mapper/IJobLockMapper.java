c146490e99c7616122bc53078b278c88470eeecd
ni <1415164057@qq.com>
2026-06-11 17:38:35 +0800

    feat: enhance workflow rerun and node management UI

--- nbatch-admin/src/main/java/com/nbatch/job/admin/mapper/IJobLockMapper.java
+++ nbatch-admin/src/main/java/com/nbatch/job/admin/mapper/IJobLockMapper.java
@@ -13,9 +13,16 @@ import org.apache.ibatis.annotations.Select;
  */
 public interface IJobLockMapper extends BaseMapper<JobLockPo> {
 
-    @Insert("insert ignore into nbatch_job_lock(lock_name) values(#{lockName})")
+    @Insert("INSERT INTO nbatch_job_lock (lock_name)\n" +
+            "SELECT #{lockName}\n" +
+            "WHERE NOT EXISTS (\n" +
+            "    SELECT 1\n" +
+            "    FROM nbatch_job_lock\n" +
+            "    WHERE lock_name = #{lockName}\n" +
+            ")")
     int insertIgnore(@Param("lockName") String lockName);
 
     @Select("select lock_name from nbatch_job_lock where lock_name = #{lockName} for update")
     JobLockPo lockByName(@Param("lockName") String lockName);
+
 }
